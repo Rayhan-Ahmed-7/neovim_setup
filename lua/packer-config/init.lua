16406507fa8a1e9c15ff268c3e6e097446d6d522
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2023-11-13 00:04:53 +0600

    :zap: html snipet added

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -21,7 +21,14 @@ return require'packer'.startup(function()
     use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use({
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!:).
+	run = "make install_jsregexp"
+    })
+    use "rafamadriz/friendly-snippets"
     use 'NvChad/nvim-colorizer.lua'
     use({
     "jose-elias-alvarez/null-ls.nvim",
@@ -35,5 +42,9 @@ return require'packer'.startup(function()
     requires = { {'nvim-lua/plenary.nvim'} }
     }
     })
+    use {
+	"windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+    }
     end)
 
