00e05e7c99352e86c8dd4c95e926c8afa41023b7
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-01-18 20:02:35 +0600

    :zap: done

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -79,24 +79,24 @@ map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 
 -- new terminals
 map("n", "<leader>h", function()
-  require("nvchad.term").new { pos = "sp" }
+  require("configs.term").new { pos = "sp" }
 end, { desc = "terminal new horizontal term" })
 
 map("n", "<leader>v", function()
-  require("nvchad.term").new { pos = "vsp" }
+  require("configs.term").new { pos = "vsp" }
 end, { desc = "terminal new vertical term" })
 
 -- toggleable
 map({ "n", "t" }, "<A-v>", function()
-  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
+  require("configs.term").toggle { pos = "vsp", id = "vtoggleTerm" }
 end, { desc = "terminal toggleable vertical term" })
 
 map({ "n", "t" }, "<A-h>", function()
-  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+  require("configs.term").toggle { pos = "sp", id = "htoggleTerm" }
 end, { desc = "terminal toggleable horizontal term" })
 
 map({ "n", "t" }, "<A-i>", function()
-  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+  require("configs.term").toggle { pos = "float", id = "floatTerm" }
 end, { desc = "terminal toggle floating term" })
 
 -- whichkey
